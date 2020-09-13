8d7e37d18f77e124a9b3ca9780dd0110b3e0dcf6
Ahmed Fayek <ahmedfayek@MacBookPros-MacBook-Pro.local>
2020-09-13 13:54:48 +0200

    add follower and user model edit views

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Ahmed Fayek on 9/10/20.
+//  Copyright © 2020 Ahmed Fayek. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
